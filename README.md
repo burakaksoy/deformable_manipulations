f420eae4183e1a001af13283a7706a3c36cbc5ea
Burak Aksoy <burakaksoy20@gmail.com>
2023-10-29 21:51:36 -0400

    test

--- README.md
+++ README.md
@@ -1,21 +1,13 @@
 # Collaborative Manipulation of Deformable Objects with Obstacle Avoidance
 This repository includes the implementation of "Collaborative Manipulation of Deformable Objects with Obstacle Avoidance" paper submitted for ICRA 2023.
 
-**CODES WILL BE AVAILABLE HERE AFTER THE REVIEW PROCESS**
 
 ## Software Architecture
 ![Software Architecture](./software_architecture.png)
-## Demo Video (Draft)
 
-### 1D Object - Rope - 1 Follower
-https://github.com/burakaksoy/deformable_manipulations/assets/34518156/ce8464da-b167-4be7-9067-ae12b17831d4
+## Video 
+![](./Collaborative%20Manipulation%20of%20Deformable%20Objects.mp4)
 
-### 1D Object - Stiff Rod - 1 Follower
-https://github.com/burakaksoy/deformable_manipulations/assets/34518156/3ead9e3b-7eeb-474e-94df-216e12d1abba
-
-### 1D Object - Rope - 2 Followers
-https://github.com/burakaksoy/deformable_manipulations/assets/34518156/4837dcc9-e205-4225-9f82-4e7126ccb51a
-
-### 2D Object - Fabric - 3 Followers
-https://github.com/burakaksoy/deformable_manipulations/assets/34518156/95d2ee4f-2c3a-4019-8ba0-e7672683b305
-
+## Installation
+On a ROS Noetic installed Ubuntu 20.04, download and run `install.bash`. It creates `catkin_ws_deformable` workspace in your `home` directory, clone the related repos, and install.
+Note that **SOME OF THE PRIVATE REPOS WILL BE MADE PUBLIC AFTER THE REVIEW PROCESS.**
---
+++ install.bash
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# Check if MY_PASSWORD is set
+if [ -z "$MY_PASSWORD" ]; then
+    echo "Password not set. Exiting..."
+    exit 1
+fi
+
+# CUSTOM RELATED
+echo $MY_PASSWORD | sudo -S apt-get install -y sshpass;
+
+echo $MY_PASSWORD | sudo -S apt-get install -y ros-noetic-rqt-ez-publisher;
+# echo $MY_PASSWORD | sudo -S apt-get install -y ros-noetic-twist-mux;
+echo $MY_PASSWORD | sudo -S apt-get install -y spacenavd;
+echo $MY_PASSWORD | sudo -S apt-get install -y ros-noetic-spacenav-node;
+echo $MY_PASSWORD | sudo -S apt-get install -y ros-noetic-tf2-sensor-msgs;
+echo $MY_PASSWORD | sudo -S apt-get install -y ros-noetic-visualization-tutorials; # rviz python bindings.
+
+# PYTHON RELATED
+echo $MY_PASSWORD | sudo -S apt-get install -y python3-pip;
+pip3 install pyserial;
+pip3 install quadprog;
+pip3 install pandas;
+pip3 install pygame;
+pip3 install scipy;
+pip3 install numpy==1.21; # needed to resolve the issue "AttributeError: module 'numpy' has no attribute 'typeDict'"
+pip3 install shapely; # needed to calculate the swarm footprint polygon and costmap parameter updater functions
+pip3 install matplotlib==3.7.3;
+
+## Building Steps
+cd;
+mkdir catkin_ws_deformable;
+cd catkin_ws_deformable;
+rm -rf {*,.*};
+
+git clone https://github.com/burakaksoy/deformable_manipulations.git .;
+cd src;
+git clone https://github.com/burakaksoy/deformable_manipulations_rope.git; # Private
+git clone https://github.com/burakaksoy/deformable_manipulations_fabric.git; # Private
+git clone https://github.com/burakaksoy/dlo_simulator_stiff_rods.git; # Private
+git clone https://github.com/burakaksoy/fabric_simulator.git; # Private
+git clone https://github.com/burakaksoy/rviz_ortho_view_controller.git;
+git clone https://github.com/facontidavide/rosbag_editor.git;
+# git clone --branch throttle-tf-repeated-data-error https://github.com/burakaksoy/geometry2.git; # to fix tf repeating data warning flooding
+
+cd ..;
+source /opt/ros/noetic/setup.bash;
+# catkin_make -DCATKIN_BLACKLIST_PACKAGES='...;...' -DCMAKE_BUILD_TYPE=Release; 
+catkin_make -DCMAKE_BUILD_TYPE=Release; 
+source devel/setup.bash;
+
+grep -qxF 'source ~/catkin_ws_deformable/devel/setup.bash' ~/.bashrc || echo 'source ~/catkin_ws_deformable/devel/setup.bash' >> ~/.bashrc;
