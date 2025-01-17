dec6b4cd0fb85f89f3ed48fb7dfc1bbea53d8fa8
Burak Aksoy <burakaksoy20@gmail.com>
2025-01-16 20:41:06 -0500

    Add topic_tf_transformers package for real experiments

--- install.bash
+++ install.bash
@@ -82,7 +82,7 @@ git clone https://github.com/burakaksoy/odio_urdf.git # Public, needed for auto-
 git clone https://github.com/burakaksoy/rviz_ortho_view_controller.git;
 git clone https://github.com/facontidavide/rosbag_editor.git;
 # git clone --branch throttle-tf-repeated-data-error https://github.com/burakaksoy/geometry2.git; # to fix tf repeating data warning flooding
-# git clone https://github.com/burakaksoy/topic_tf_transformers.git; # to read odom from tf
+git clone https://github.com/burakaksoy/topic_tf_transformers.git; # to read odom from tf
 
 
 cd ..;
